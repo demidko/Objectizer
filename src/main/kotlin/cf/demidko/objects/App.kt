2854e3be1697501c4c6f2e82e066b037bafd37ac
Daniel Demidko <demidko@farpost.com>
2020-09-18 02:17:06 +1000

    Базовый концепт

--- src/main/kotlin/cf/demidko/objects/App.kt
+++ src/main/kotlin/cf/demidko/objects/App.kt
@@ -1,7 +1,16 @@
 package cf.demidko.objects
 
 
-fun main() {
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.time.Duration
+import java.time.Duration.ofDays
 
+
+fun main() {
+    val storage = mutableMapOf<Duration, List<Object>>()
+    storage[ofDays(1)] = listOf(Object("rt"), Object("dd"))
+    val mapper = ObjectMapper()
+
+    println(mapper.writeValueAsString(storage))
 }
 
